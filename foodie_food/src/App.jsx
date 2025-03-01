141febcd9f3c521a51f0a2a476ab1e232915ee05
Rahil1910 <rahilrafique1234@gmail.com>
2025-03-02 02:19:08 +0530

    first commit

---
+++ foodie_food/src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import "./app.css";
+import Home from './pages/home';
+import YourOrder from "./pages/yourOrder";
+import { useState } from "react";
+import { use } from "react";
+
+function App() {
+  const [orders , setOrders] = useState([]);
+  useEffect(()=>{
+    console.log(orders)
+  },[orders])
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home setOrders = {setOrders}/>}/>
+          <Route path="/yourOrder" element={<YourOrder orders = {orders} setOrders = {setOrders}/>}></Route>
+        </Routes>
+      </BrowserRouter>
+  )
+}
+
+export default App
